82230c8b6678e0bf9b77709d81581bc3e964e5b6
El-Zieny <abdoelzieny72@gmail.com>
2023-11-29 11:50:27 +0200

    binary_trees

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,12 +7,13 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves;
+
 	if (!tree)
 		return (0);
 
 	if (!tree->left && !tree->right)
 		return (1);
 	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
-	
+
 	return (leaves);
 }
