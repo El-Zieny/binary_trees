f6c0679f74b06fd485ff37e9d035c2c623411654
El-Zieny <abdoelzieny72@gmail.com>
2023-11-29 08:22:08 +0200

    binary_trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_h, right_h;
 
 	if (!tree)
-		return(0);
+		return (0);
 
 	left_h = 0;
 	right_h = 0;
@@ -19,7 +19,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree->right)
 		right_h = 1 + binary_tree_height(tree->right);
-	
+
 	if (left_h > right_h)
 		return (left_h);
 	return (right_h);
